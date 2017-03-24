932c5e5a0e5d113661fa3fc7f30ea37c45a5773d
isperLucas <isperifsp@gmail.com>
2017-03-24 16:48:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,192 @@
+// Valida Login
+function validacaoLogin(){
+    $('#errorLogin').hide();
+
+    var email = $('#email_login').val();
+    var senha = $('#senha_login').val();
+
+    var request = $.ajax({
+        url: "ajaxLogin.php",
+        method: "POST",
+        data: { emailUser_login : email, senhaUser_login : senha},
+        dataType: "json"
+    });
+    
+    request.done(function( data ) {
+        if(data.error == 1){
+          $('#errorLogin').fadeIn();
+        
+        }else{          
+          window.location = "usuario.php";
+        }
+    }); 
+
+}
+
+// Valida Cadastro
+function validar(){
+	if (document.formu.nome.value==""){
+		alert("campo nome é obrigatório");
+		document.formu.nome.focus();
+		return false;
+	}
+	else if (document.formu.nome.value.length<4){
+		alert("O campo nome deve conter no mínimo 4 caracteres.");
+		document.formu.nome.focus();
+		return false;
+	}
+	else if(document.formu.nick.value==""){
+		alert("campo nick obrigatório");
+		document.formu.nick.focus();
+		return false;
+		
+	}
+	else if (document.formu.nick.value.length<5){
+		alert("o campo nick deve conter no mínimo 5 dígitos");
+		document.formu.nick.focus();
+		return false;
+		
+	}
+	else if (document.formu.email.value==""){
+		alert("Campo email obrigatório");
+		document.formu.email.focus();
+		return false;
+	}
+	else if (document.formu.senha.value=="" || document.formu.senha.value.length<5){
+		alert("A senha deve conter no mínimo 5 dígitos");
+		document.formu.senha.focus();
+		return false;
+	}else if (document.formu.confirmarSenha.value==""){
+		alert("Confirme a senha!");
+		document.formu.confirmarSenha.focus();
+		return false;
+		
+	}
+	else if (document.formu.senha.value != document.formu.confirmarSenha.value){
+		alert("As senhas devem ser iguais");
+		document.formu.confirmarSenha.focus();
+		return false
+		
+	}else {
+		return true;
+	}
+}
+
+/*
+ * jQuery appear plugin
+ *
+ * Copyright (c) 2012 Andrey Sidorov
+ * licensed under MIT license.
+ *
+ * https://github.com/morr/jquery.appear/
+ *
+ * Version: 0.3.6
+ */
+
+(function($) {
+  var selectors = [];
+
+  var check_binded = false;
+  var check_lock = false;
+  var defaults = {
+    interval: 250,
+    force_process: false
+  };
+  var $window = $(window);
+
+  var $prior_appeared = [];
+
+  function appeared(selector) {
+    return $(selector).filter(function() {
+      return $(this).is(':appeared');
+    });
+  }
+
+  function process() {
+    check_lock = false;
+    for (var index = 0, selectorsLength = selectors.length; index < selectorsLength; index++) {
+      var $appeared = appeared(selectors[index]);
+
+      $appeared.trigger('appear', [$appeared]);
+
+      if ($prior_appeared[index]) {
+        var $disappeared = $prior_appeared[index].not($appeared);
+        $disappeared.trigger('disappear', [$disappeared]);
+      }
+      $prior_appeared[index] = $appeared;
+    }
+  }
+
+  function add_selector(selector) {
+    selectors.push(selector);
+    $prior_appeared.push();
+  }
+
+  // "appeared" custom filter
+  $.expr[':'].appeared = function(element) {
+    var $element = $(element);
+    if (!$element.is(':visible')) {
+      return false;
+    }
+
+    var window_left = $window.scrollLeft();
+    var window_top = $window.scrollTop();
+    var offset = $element.offset();
+    var left = offset.left;
+    var top = offset.top;
+
+    if (top + $element.height() >= window_top &&
+        top - ($element.data('appear-top-offset') || 0) <= window_top + $window.height() &&
+        left + $element.width() >= window_left &&
+        left - ($element.data('appear-left-offset') || 0) <= window_left + $window.width()) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  $.fn.extend({
+    // watching for element's appearance in browser viewport
+    appear: function(options) {
+      var opts = $.extend({}, defaults, options || {});
+      var selector = this.selector || this;
+      if (!check_binded) {
+        var on_check = function() {
+          if (check_lock) {
+            return;
+          }
+          check_lock = true;
+
+          setTimeout(process, opts.interval);
+        };
+
+        $(window).scroll(on_check).resize(on_check);
+        check_binded = true;
+      }
+
+      if (opts.force_process) {
+        setTimeout(process, opts.interval);
+      }
+      add_selector(selector);
+      return $(selector);
+    }
+  });
+
+  $.extend({
+    // force elements's appearance check
+    force_appear: function() {
+      if (check_binded) {
+        process();
+        return true;
+      }
+      return false;
+    }
+  });
+})(function() {
+  if (typeof module !== 'undefined') {
+    // Node
+    return require('jquery');
+  } else {
+    return jQuery;
+  }
+}());
---
+++ topo.php
@@ -0,0 +1,156 @@
+<?php
+    require_once 'classes/usuario.class.php';
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>IndieGames</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap Css -->
+<link rel="stylesheet" href="bootstrap/dist/css/bootstrap.css" >    
+
+<!-- Bootstrap JQuery -->
+<script src="bootstrap/dist/js/jquery.js"></script>  
+
+<!-- Bootstrap Java Script -->
+<script src="bootstrap/dist/js/bootstrap.js"></script>    
+
+<!-- Bootstrap Fonts --> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+<!-- Style Css -->   
+<link rel="stylesheet" href="css/styles.css">
+    
+<!-- Scripts -->   
+<script src="js/script.js"></script>
+    
+<link rel="shortcut icon" href="img/logov4.ico" type="image/x-icon" />    
+
+<script async="async" src="https://www.googletagservices.com/tag/js/gpt.js"></script>
+
+</head>
+<body>
+    <div id="bannerHome">
+    <img class="img-responsive" src="img/logov4_capa2.png" >
+    </div>
+
+    <!-- Nav -->
+    <nav class="navbar navbar-inverse" id="navBar">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                <a class="navbar-brand" href="#">Jogos</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Desenvolvedores</a></li>
+                    <li><a href="#">Cadastrar</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right" >
+                    <?php
+                        if(isset($_SESSION['logado']) == "sim"){
+                            echo "<li id='btnModalCad' data-toggle='modal' data-target='#modalCad'><a href='#'><span class='glyphicon glyphicon-user'></span>Lucas</a></li>";
+                        }else{
+                            echo "<li id='btnModalCad' data-toggle='modal' data-target='#modalCad'><a href='#'><span class='glyphicon glyphicon-user'></span>Cadastrar</a></li>";
+                            
+                            echo "<li id='btnModalLog' data-toggle='modal' data-target='#modalLog'><a href='#'><span class='glyphicon glyphicon-user'></span> Entrar</a></li>";
+                        };
+                    ?>
+                </ul>    
+            </div>
+        </div>
+    </nav><!--Nav -->
+    
+    <!-- Modal Cadastro-->
+    <div class="modal fade" id="modalCad" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header" style="padding:35px 50px;">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h5><span class="glyphicon glyphicon-lock"></span> Cadastro</h5>
+                </div>
+                <div class="modal-body" style="padding:40px 50px;">
+                    <form role="form"  method="post" name="formu">
+                        <div class="form-group">
+                            <label for="usrname"><span class="glyphicon glyphicon-user"></span> Nome Completo:</label>
+                            <input type="text" class="form-control" name="nome" id="usrname" placeholder="Nome">
+                        </div>
+                        <div class="form-group">
+                            <label for="nickname"><span class="glyphicon glyphicon-user"></span> Nome de Usuário:</label>
+                            <input type="text" class="form-control" name="nick" id="nickname" placeholder="Usuário">
+                        </div>
+                        <div class="form-group">
+                            <label for="email"><span class="glyphicon glyphicon-envelope"></span> Email:</label>
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Senha</label>
+                            <input type="password" class="form-control" name="senha" id="psw" placeholder="password">
+                        </div>
+                        <div class="form-group">
+                            <label for="psw"><span class="glyphicon glyphicon-eye-open"></span>Confirmar Senha</label>
+                            <input type="password" class="form-control" name="confirmarSenha" id="psw" placeholder="password">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" onclick="return validar();" class="btn btn-default btn-block entrar" name="btnCadastro">  <span class="glyphicon glyphicon-off"></span> Entrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div><!-- .modal Cadastro -->
+
+    <!-- Modal Login -->
+    <div class="modal fade" id="modalLog" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header" style="padding:35px 50px;">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h5><span class="glyphicon glyphicon-lock"></span> Entrar</h5>
+                </div>
+                <div class="modal-body" style="padding:40px 50px;">
+                    <form role="form" method="post">
+                        <div class="form-group">
+                            <label for="usrname"><span class="glyphicon glyphicon-user"></span> Usuário</label>
+                            <input type="text" class="form-control" name="emailUser_login" id="email_login" placeholder="Nome do Usuário">
+                        </div>
+                        <div class="form-group">
+                            <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Senha</label>
+                            <input type="password" class="form-control" name="senhaUser_login" id="senha_login" placeholder="Senha"> 
+                            <div class="alert alert-danger alert-block alert-aling" role="alert" style="display:none" id="errorLogin">Ops! E-mail ou Senha estão errado</div>
+                        </div>
+                        <div class="checkbox">
+                            <label><input type="checkbox" value="" checked>Lembrar de mim</label>
+                        </div>
+                        <span onclick="validacaoLogin();" name="btLogar" class="btn btn-default btn-block entrar"><span class="glyphicon glyphicon-off"></span> Entrar</span>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-default pull-left cancelar" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancelar</button>
+                    <p>Não é um membro? <a href="#">Cadastre-se</a></p>
+                    <p>Esqueceu a <a href="#">Senha?</a></p>
+                </div>
+            </div>
+        </div>
+    </div><!-- .modal Login --> 
+ 
+    <script>
+   $(document).scroll(function(){
+        if($('#bannerHome').is(':appeared')){
+            $("#navBar").removeClass("navbar-fixed-top");
+        } else{
+            $("#navBar").addClass("navbar-fixed-top");
+        }
+    });
+          
+   
+    </script>        
