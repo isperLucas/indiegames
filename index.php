b1397e0356b383031f8c6ca12323d0ce5f803221
jonasifsp <jonasifsp@gmail.com>
2017-04-03 15:00:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,12 @@ include "templates/topo.php";
   <div class="jumbotron">
     <h1>Indie Games</h1> 
     <p> Aqui você pode, postar e baixar jogos independentes de graça </p> 
+	<p> Em breve, aguarde...</p>
   </div>
 </div>
 
 <div class="container-fluid text-center">
-    <h3>Jogos em Destaque </h3>
+    <h3>Jogos em Destaque </h3><br>
     
     <div class="col-xs-12 col-sm-6 col-md-3">
         <div class="thumbnail">
@@ -47,10 +48,17 @@ include "templates/topo.php";
 
     <div class="col-xs-12 col-sm-6 col-md-3">
         <div class="thumbnail">
-             <a href="#"><img src="img/jogo3.png" class="img-responsive" style="width:100%" alt="Image"></img>
-            <p>Labirinto Cinza</p></a> 
+             <a href="#"><img src="img/jogo4.png" class="img-responsive" style="width:100%" alt="Image"></img>
+            <p>Malarque X</p></a> 
         </div>
     </div>
 </div>
+
+<div class="container text-center">    
+  <h3> Cadastre-se para postar e baixar jogos Indie </h3><br>
+   <button class="btn btn-default" data-toggle="modal" data-target="#modalCad">Cadastrar</button><br>
+  </div>
+	</div><br><br>
+
 
 <?php include "templates/footer.php";?>
